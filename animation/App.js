3c6ee522b6df0924e71ecddd6708917d328aef5f
DanielGhanabari <danielghanbari7@gmail.com>
2022-08-14 17:01:02 +0100

    Bouncing Ball Animation

--- animation/App.js
+++ animation/App.js
@@ -1,20 +1,67 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import {PanGestureHandler} from 'react-native-gesture-handler';
 
+const SIZE = 60;
 const App = () => {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const panGestureEvent = useAnimatedGestureHandler({
+    onStart: (e, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (e, context) => {
+      translateX.value = e.translationX + context.translateX;
+      translateY.value = e.translationY + context.translateY;
+    },
+    onEnd: () => {
+      translateX.value = withSpring(0);
+      translateY.value = withSpring(0);
+    },
+  });
+
+  const animStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <PanGestureHandler onGestureEvent={panGestureEvent}>
+        <Animated.View style={[styles.container, animStyle]}>
+          <Animated.View style={[styles.object, animStyle]}></Animated.View>
+        </Animated.View>
+      </PanGestureHandler>
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  object: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: 'red',
+    borderRadius: 50,
+    margin: 5,
   },
 });
 
